91dc2a881deefebc0076571bc0b7ad1acbf3b70c
Matt Micene (RHT) <mmicene@redhat.com>
2025-10-24 13:36:14 -0400

    Test action

--- setup-automation/setup-rhel.sh
+++ setup-automation/setup-rhel.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+echo "This is an arbitrary change that serves no purpose other than a git commit"
+
 # This setup is a no-op, no original setup needed
 echo "DONE" >> /root/post-run.log
